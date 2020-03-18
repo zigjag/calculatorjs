86151be7797466ccbb2434fc4f4a134415ceb650
Joseph Kligel <jkligel@gmail.com>
2020-03-17 23:27:31 -0500

    Using bash script to push all updates

--- calculator.js
+++ calculator.js
@@ -15,6 +15,21 @@ app.post("/", function(req, res) {
   res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+  var wt = Number(req.body.weight);
+  var ht = Number(req.body.height);
+  var BMI = wt/Math.pow(ht, 2);
+  res.send("Your BMI is " + roundToTwo(BMI));
+});
+
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
